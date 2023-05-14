9ffe9d50f6c11369f3f37bf16acfdcfeedd68866
jackfiled <xcrenchangjun@outlook.com>
2023-05-14 13:55:48 +0800

    add: 化简器接口 自述文件

---
+++ src/main/java/top/rrricardo/cfgs/simplifiers/Simplifier.java
@@ -0,0 +1,15 @@
+package top.rrricardo.cfgs.simplifiers;
+
+import top.rrricardo.cfgs.models.ContextFreeGrammar;
+
+/**
+ * 需要实现的化简接口
+ */
+public interface Simplifier {
+    /**
+     * 化简指定的上下文无关语法
+     * @param grammar 需要化简的语法
+     * @return 化简之后的结果
+     */
+    ContextFreeGrammar simplify(ContextFreeGrammar grammar);
+}
